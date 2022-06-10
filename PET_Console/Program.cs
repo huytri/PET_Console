584a567a2f8052c623dc5038313d61d6792cc514
Tri (Triton) H.NGUYEN <thnguyen@gosecure.net>
2022-06-11 05:54:51 +0700

    Initial commit

---
+++ PET_Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PET_Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 6, 7, 8, 4, 1, 9, -3};
+            var result = SortOddEven(numbers);
+            
+            List<int> SortOddEven(int[] nums)
+            {
+                List<int> oddEven = new List<int>();
+                List<int> sortedList = new List<int>();
+                
+                foreach (var num in nums)
+                {
+                    oddEven.Add(num);
+                }
+                    
+                oddEven.Sort();
+                return sortedList;
+            }
+        }
+    }
+}
